211e03c108895a2a55f10e146d81b3db847e2060
6teendollars <91662992+6teendollars@users.noreply.github.com>
2023-02-16 18:30:48 +0100

    task2
    task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,41 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulRef = document.querySelector("ul");
+
+
+//========первый спосою==================
+
+
+// const liListRef = ingredients.map(name => {
+//   const liRef = document.createElement('li')
+//   liRef.textContent = name
+//   return liRef
+// })
+// li.classList.add('item')
+
+// ulRef.append(...liListRef)
+// li.className = 'item'
+// console.log(ulRef);
+// document.body.append(ulRef)
+
+
+//=============второй способ===========
+
+ingredients.reduce((acc, name) => {
+  const liRef = document.createElement("li");
+  liRef.classList.add("item");
+
+  liRef.textContent = name;
+  acc.appendChild(liRef);
+  return acc;
+}, ulRef);
+
+// console.log(result);
+console.log(ulRef);
